b03a80f6bc771106a64b153b194af52ecd9cce8c
elianiva <dicha.arkana03@gmail.com>
2020-08-07 18:38:27 +0700

    fix

--- index.ts
+++ index.ts
@@ -6,9 +6,7 @@ function start(client: Client) {
   client.onMessage(async (message: Message) => {
     if (message.caption === "#sticker" && message.mimetype) {
       console.log("Loading...")
-      const filename: string = `${message.t}.${mime.extension(
-        message.mimetype
-      )}`
+      const filename: string = `${message.t}.${mime.extension(message.mimetype)}`
 
       try {
         console.log("Decrypting...")
@@ -16,9 +14,9 @@ function start(client: Client) {
         const imageBase64: string = `data:${
           message.mimetype
         };base64,${mediaData.toString("base64")}`
-
-        console.log("sticker sent!")
+       
         client.sendImageAsSticker(message.from, imageBase64)
+        console.log("sticker sent!")
       } catch (err) {
         throw new Error(err.message)
       }
