14c916243f5b33c840e4d3ebdb8b776d2d5738a4
elianiva <dicha.arkana03@gmail.com>
2020-08-07 18:12:44 +0700

    Initial Commit

---
+++ index.ts
@@ -0,0 +1,29 @@
+import { create, Client, decryptMedia, Message } from "@open-wa/wa-automate"
+import mime from "mime-types"
+import fs from "fs"
+
+function start(client: Client) {
+  client.onMessage(async (message: Message) => {
+    if (message.caption === "#sticker" && message.mimetype) {
+      console.log("Loading...")
+      const filename: string = `${message.t}.${mime.extension(
+        message.mimetype
+      )}`
+
+      try {
+        console.log("Decrypting...")
+        const mediaData = await decryptMedia(message)
+        const imageBase64: string = `data:${
+          message.mimetype
+        };base64,${mediaData.toString("base64")}`
+
+        console.log("sticker sent!")
+        client.sendImageAsSticker(message.from, imageBase64)
+      } catch (err) {
+        throw new Error(err.message)
+      }
+    }
+  })
+}
+
+create().then((client: Client) => start(client))
